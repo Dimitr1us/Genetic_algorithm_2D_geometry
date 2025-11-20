ea1766e7e46b852c7d774279271a42257a8d39f3
Dimitrius <speed.Dima05@gmail.com>
2025-11-20 17:18:10 +0300

    Added new Side.cs from Shape.cs

--- Genetic Algorithms/Shapes.cs
+++ Genetic Algorithms/Shapes.cs
@@ -6,46 +6,11 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using PointLib;
+using SideLib;
 using Point = PointLib.Point;
 namespace ShapesLib
 {
-    public class Side
-    {
-        private Point point1, point2;
-        public Side(Point point1, Point point2)
-        {
-            this.point1 = point1;
-            this.point2 = point2;
-        }
-
-        public float Length()
-        {
-            return (float)Math.Sqrt(Math.Pow(point1.Horisontal() - point2.Horisontal(), 2) + Math.Pow(point1.Vertical() - point2.Vertical(), 2));
-        }
-
-        public void Move(float x, float y)
-        {
-            point1.Move(x, y);
-            point2.Move(x, y);
-        }
-
-        public float Distance(Point point)
-        {
-            return point1.Distance(point) + point2.Distance(point);
-        }
-
-        public Point FirstPoint()
-        {
-            return point1;
-        }
-
-        public Point SecondPoint()
-        {
-            return point2;
-        }
-    }
-
-
+   
     public interface iShape
     {
         public void Move(float x, float y);
---
+++ Genetic Algorithms/Side.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PointLib;
+using Point = PointLib.Point;
+namespace SideLib
+{
+    public class Side
+    {
+        private Point point1, point2;
+        public Side(Point point1, Point point2)
+        {
+            this.point1 = point1;
+            this.point2 = point2;
+        }
+
+        public float Length()
+        {
+            return (float)Math.Sqrt(Math.Pow(point1.Horisontal() - point2.Horisontal(), 2) + Math.Pow(point1.Vertical() - point2.Vertical(), 2));
+        }
+
+        public void Move(float x, float y)
+        {
+            point1.Move(x, y);
+            point2.Move(x, y);
+        }
+
+        public float Distance(Point point)
+        {
+            return point1.Distance(point) + point2.Distance(point);
+        }
+
+        public Point FirstPoint()
+        {
+            return point1;
+        }
+
+        public Point SecondPoint()
+        {
+            return point2;
+        }
+    }
+}
